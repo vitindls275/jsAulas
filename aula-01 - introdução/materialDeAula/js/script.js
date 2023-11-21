90d416ff041e47db155f2bbcab2a727528ab164e
vitindls275 <Victorsouzaribeiro20@gmail.com>
2023-11-21 17:54:04 -0300

    aula-01 - javascript

---
+++ aula-01 - introdução/materialDeAula/js/script.js
@@ -0,0 +1,28 @@
+console.log("helo word");
+
+// CAIXAS DE TEXTO
+function testLet(params) {
+    
+}
+alert("ola, esse é meu primeiro comando js!")
+confirm("Estou aprendendon js?")
+prompt("qual é o seu nome?")
+
+// VARIAVEIS
+
+// LET = escopo local, sobrescrevivel
+let numero = 10
+console.log
+
+//  CONST = escopo global, não sobrescrevivel
+
+
+
+// vVAR escopo global, sobrescrevivel
+
+var nome = "Victor"
+console.log()
+
+var idade = pronpt ("qualé a sua idade")
+alert(nome + "seja bem vindo")
+ 
