e9b6ccd0fc51e8819fc164a1cea6e1549b664d3a
vitindls275 <Victorsouzaribeiro20@gmail.com>
2023-12-05 17:50:08 -0300

    aula-10

--- aula-10  - jquery animação/materialDeAula/js/script.js
+++ aula-10  - jquery animação/materialDeAula/js/script.js
@@ -1,6 +1,23 @@
-var caixa =$("#caixa");
+var box =$("#box");
 
-$("animation1").on('click', function(){
-    caixa.animate({width : 500px, heigth : 500px})
-
+$("#animation1").on('click', function(){
+    box.animate({width : '500px', height : '500px', borderRadius : '100%'})
 })
+
+$("#animation2").on('click', function(){
+    box.animate({width : '300px', height : '300px', rotate : '360deg', duration : 1000 })
+}) 
+$("#animation3").on('click', function(){
+    box.animate({width : '300px', height : '300px', rotate : '-720deg', duration : 5000 })
+}) 
+$("#animation4").on('click', function(){
+    box.animate({})
+}) 
+
+$("#animation4").on('click', function(){
+    box.animate({})
+}) 
+
+$("#animation5").on('click', function(){
+    box.animate({})
+}) 
